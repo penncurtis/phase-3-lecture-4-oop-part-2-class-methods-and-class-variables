bc7c856630bcae344584bf286ea610f5e6924bc4
Penn Curtis <penn.s.m.c@gmail.com>
2023-05-25 17:01:22 -0400

    complete

--- lib/dog.py
+++ lib/dog.py
@@ -1,5 +1,16 @@
 import ipdb
 from animal import *
 
-class Dog:
-    pass
+class Dog(Animal):
+
+    all = []
+
+    def __init__(self, name, age, bark_volume):
+        self.name = name
+        self.age = age
+        self.bark_volume = bark_volume
+        Dog.all.append(self)
+
+    
+    def make_animal_sound(self):
+        print("Bark!")
--- lib/run.py
+++ lib/run.py
@@ -2,3 +2,13 @@ import ipdb
 from animal import *
 from dog import *
 from cat import *
+
+animal1 = Animal("Fluffy", 4)
+animal2 = Animal("Kitty", 2)
+
+dog1 = Dog("Fido", 13)
+
+
+Animal.get_all_animal_names()
+
+ipdb.set_trace()
